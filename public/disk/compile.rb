2709bf85a6637760f1afdcb58321242595f4f09d
Myles Skinner <myles.skinner@gmail.com>
2020-07-21 01:30:22 -0400

    Ruby compiler script; refactor all software as .src/.obj files

--- public/disk/compile.rb
+++ public/disk/compile.rb
@@ -5,20 +5,29 @@ class Compiler
     @symbols = {}
     @labels = {}
     @address = 0
+
+    if ARGV[0]
+      @filename = ARGV[0]
+    else
+      puts 'Usage: ruby compile.rb <filename>'
+      exit
+    end
   end
 
+  # Have to do this funky key sort because any symbol/label
+  # can be a substring of another
   def resolve_symbols(part)
-    @symbols.each { |k, v| part.gsub!(k, v) } 
+    @symbols.sort_by {|k, _| k.length }.reverse.each { |k, v| part.gsub!(k, v) } 
     part
   end
 
   def resolve_labels(part)
-    @labels.each { |k, v| part.gsub!(k, "$#{v.to_s(16)}") }
+    @labels.sort_by {|k, _| k.length }.reverse.each { |k, v| part.gsub!(k, "$#{v.to_s(16)}") }
     part
   end
 
   def pass_one
-    File.open("./hello.src", 'r') do |file|
+    File.open("./#{@filename}.src", 'r') do |file|
       file.each do |line|
         parts = line.split(' ')
 
@@ -32,7 +41,13 @@ class Compiler
           end
         when ' '
           opcode = parts[0]
-          operand = resolve_symbols(parts[1]) unless parts[1].nil?
+          if opcode == '!str'
+            operand = line.match(/`(.*?)`/).to_s.gsub('`', '') + "\0"
+          elsif opcode == '!chr'
+            operand = line.match(/`(.*?)`/).to_s.gsub('`', '')
+          else
+            operand = resolve_symbols(parts[1]) unless parts[1].nil? || parts[1][0] == ';'
+          end
 
           @source_lines << {}.tap do |line|
             line[:opcode]  = opcode.strip
@@ -44,7 +59,13 @@ class Compiler
           @source_lines << line
         when /\p{Alpha}/
           opcode = parts[1]
-          operand = resolve_symbols(parts[2]) unless parts[2].nil?
+          if opcode == '!str'
+            operand = line.match(/`(.*?)`/).to_s.gsub('`', '') + "\0"
+          elsif opcode == '!chr'
+            operand = line.match(/`(.*?)`/).to_s.gsub('`', '')
+          else
+            operand = resolve_symbols(parts[2]) unless parts[2].nil? || parts[2][0] == ';'
+          end
 
           @source_lines << {}.tap do |line|
             line[:label]   = parts[0]
@@ -82,6 +103,15 @@ class Compiler
     end
   end
 
+  def string_to_memory(part)
+    part.scan(/.{1,8}/).each do |chunk|
+      my_string = "> #{@address.to_s(16)}"
+      chunk.chars.each { |char| my_string << " #{'%02X' % char.ord}" }
+      @object_lines << "#{my_string}\n"
+      @address += chunk.size
+    end
+  end
+
   def pass_two
     @source_lines.each do |line|
       if line.is_a? String
@@ -89,11 +119,20 @@ class Compiler
       else
         if line[:set_address].to_i > 0
           @address = line[:set_address]
+        elsif line[:opcode] == '!str' || line[:opcode] == '!chr'
+          @labels[line[:label]] = @address unless line[:label].nil?
+          string_to_memory(line[:operand])
         else
           @labels[line[:label]] = @address unless line[:label].nil?
-          
+
+          i = line[:operand].index('+') if line[:operand]
+          if i
+            value = line[:operand][i-4..i-1].to_i(16) + line[:operand][i+1..].to_i(16)
+            line[:operand] = line[:operand][0..i-5] + value.to_s(16)
+          end
+
           if line[:opcode]
-            @object_lines << ". #{@address.to_s(16)} #{line[:opcode]} #{line[:operand]}".strip
+            @object_lines << ". #{@address.to_s(16)} #{line[:opcode]} #{line[:operand]}".strip + "\n"
             @address += bytes(line[:opcode], line[:operand])
           end
         end
@@ -106,21 +145,16 @@ class Compiler
   end
 
   def write_object_code
-    @object_lines.each do |line|
-      # if line.is_a? String
-        puts line
-      # else
-      #   puts ". #{line[:label]} #{line[:instruction]}".strip
-      # end
+    File.open("./#{@filename}.obj", 'w') do |file|
+      @object_lines.each do |line|
+        file.write line
+      end
     end
-    puts @labels.inspect
   end
 end
 
 c = Compiler.new
-puts c.inspect
 c.pass_one
 c.pass_two
 c.pass_three
 c.write_object_code
-puts 'Done.'
